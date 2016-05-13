38dbd4803a3929bddb36d0d58fbf69485985645a
Dominic Williams <Dominic@Dominics-MacBook-Pro.local>
2016-05-13 14:28:19 -0400

    Created Random Restaurant app

--- What to eat?/ViewController.swift
+++ What to eat?/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var restaurantLabel: UILabel!
+    @IBOutlet weak var inputRestaurantField: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    var restaurantsArray: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.errorLabel.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func addRestaurantPressed(sender: AnyObject) {
+        if self.inputRestaurantField.text != ""{
+            if self.errorLabel.hidden == false {
+                self.errorLabel.hidden = true
+            }
+            self.restaurantsArray.append(self.inputRestaurantField.text!)
+            self.inputRestaurantField.text = ""
+        } else {
+            self.errorLabel.hidden = false
+        }
+    }
+    
+    @IBAction func randomizeRestaurantPressed(sender: AnyObject) {
+        let randomIndex = Int(arc4random_uniform(UInt32(self.restaurantsArray.count)))
+        self.restaurantLabel.text = self.restaurantsArray[randomIndex]
+    }
 
 }
 
