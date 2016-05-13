2183d17c9295a227e9e6d1a642501afb47fb5a00
Dominic Williams <Dominic@dominics-macbook-pro.local>
2016-05-13 13:24:36 -0400

    Initial Commit

---
+++ What to eat?Tests/What_to_eat_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  What_to_eat_Tests.swift
+//  What to eat?Tests
+//
+//  Created by Dominic Williams on 5/13/16.
+//  Copyright © 2016 Dominic Williams. All rights reserved.
+//
+
+import XCTest
+@testable import What_to_eat_
+
+class What_to_eat_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
